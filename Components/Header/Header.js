98172032ee7f8be86fc9b457d672a64737d71f0f
Ashik045 <ashikurislam045@gmail.com>
2022-11-08 10:26:20 +0600

    start working

--- Components/Header/Header.js
+++ Components/Header/Header.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
-const Header = () => {
-  return (
-    <div>Header</div>
-  )
+function Header() {
+    return <div>Header</div>;
 }
 
-export default Header
+export default Header;
--- Components/Navbar/Navbar.js
+++ Components/Navbar/Navbar.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
 
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+function Navbar() {
+    return (
+        <div>
+            <div className="navbar_main">
+                <div className="brand">
+                    <h3>Ashikur</h3>
+                </div>
+
+                <div className="menus">
+                    <p>Home</p>
+                    <p>About</p>
+                    <p>Portfolio</p>
+                    <p>Contact</p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default Navbar
+export default Navbar;
